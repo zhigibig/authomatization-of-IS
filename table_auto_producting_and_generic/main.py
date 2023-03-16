65f8c7659d25b202474d93be2d72b7039f4b92b7
Gamzat Shakhmanaev <95529471+zhigibig@users.noreply.github.com>
2023-03-16 21:19:15 +1300

    Update main.py

--- table_auto_producting_and_generic/main.py
+++ table_auto_producting_and_generic/main.py
@@ -1,15 +1,26 @@
 import pandas as pd
-import numpy as np
-import sqlite3
+from data import materials
+from random import randint
 
-def data_export():
+def table_gen(LENGTH_OF_TABLE = 50):
+    """This function is generating table in format dict().
+    """
+    table = dict()
     
+    for i in materials:
+        table[f'{i}'] = [randint(1, 20) for i in range(LENGTH_OF_TABLE)]
+
+    return table
+
+def main() -> int:
+
+    excel_table = table_gen()
+
+    df = pd.DataFrame(excel_table)
+
+    df.to_excel("./Склад_1.xlsx")
+
     return 0
-
-def main():
-
-    return 0 
 
 if __name__ == '__main__':
     main()
-
