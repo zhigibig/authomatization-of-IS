14bdbaee372caa36dcf9609b60ce5fc2280d8204
zhigibig <gamzat0004@mail.ru>
2023-03-16 10:24:48 +0300

    first commit

---
+++ table_auto_producting_and_generic/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import sqlite3
+
+def data_export():
+    
+    return 0
+
+def main():
+
+    return 0 
+
+if __name__ == '__main__':
+    main()
+
---
+++ table_auto_producting_and_generic/materials_in_stock_sheet_table_gen.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from data import materials
+
+
+
+def table_gen():
+    global table 
+    table = dict()
+
+    for k in table:
+        pass
+
+    return 0
+
+def data_export():
+    return 0
+
+def main():
+
+    return 0
+
+if __name__ == '__main__':
+    main()
